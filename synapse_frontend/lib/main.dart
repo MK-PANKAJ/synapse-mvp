59fb6d275667e9b6491080ab3b10df87e5ef7ad0
MANISH KUMAR PANKAJ <manishkumarpankaj119@gmail.com>
2026-01-03 23:30:59 +0530

    Fix: Add missing dart:html import for web support

--- synapse_frontend/lib/main.dart
+++ synapse_frontend/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:html' as html;
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:url_launcher/url_launcher.dart';
